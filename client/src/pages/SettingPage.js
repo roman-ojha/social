1c958568b247a09427ebd3a29638128f10ad88cc
Roman Ojha <razzroman98@gmail.com>
2022-03-13 22:36:27 +0545

    getting setting page input filed data on as state

--- client/src/pages/SettingPage.js
+++ client/src/pages/SettingPage.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Helmet } from "react-helmet";
 import "../styles/pages/settingPage.css";
 
 const SettingPage = () => {
+  const [settingInputFieldData, setSettingInputFieldData] = useState({
+    userID: "",
+    name: "",
+    oldPassword: "",
+    newPassword: "",
+    cNewPassword: "",
+  });
+  const getInputFieldData = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setSettingInputFieldData({
+      ...settingInputFieldData,
+      [name]: value,
+    });
+  };
+  const changeUserID = () => {};
+  const changeName = () => {};
+  const chnagePassword = () => {};
+  const deleteUser = () => {};
   return (
     <>
       <div className="SettingPage_Container">
@@ -12,26 +31,56 @@ const SettingPage = () => {
         <div className="Setting_Container_With_Input_Field">
           <h1>Change UserID</h1>
           <form>
-            <input type="text" placeholder="UserID" />
-            <button>Change</button>
+            <input
+              type="text"
+              placeholder="UserID"
+              name="userID"
+              value={settingInputFieldData.userID}
+              onChange={getInputFieldData}
+            />
+            <button onClick={changeUserID}>Change</button>
           </form>
           <p>You can only be able to set unique ID for your profile</p>
         </div>
         <div className="Setting_Container_With_Input_Field">
           <h1>Change display name</h1>
           <form>
-            <input type="text" placeholder="Display Name" />
-            <button>Change</button>
+            <input
+              type="text"
+              placeholder="Display Name"
+              name="name"
+              value={settingInputFieldData.name}
+              onChange={getInputFieldData}
+            />
+            <button onClick={changeName}>Change</button>
           </form>
           <p>Not require to be unique</p>
         </div>
         <div className="Setting_Container_With_Input_Field">
           <h1>Change Password</h1>
-          <input type="password" placeholder="Old password" />
-          <input type="password" placeholder="New password" />
+          <input
+            type="password"
+            placeholder="Old password"
+            name="oldPassword"
+            value={settingInputFieldData.oldPassword}
+            onChange={getInputFieldData}
+          />
+          <input
+            type="password"
+            placeholder="New password"
+            name="newPassword"
+            value={settingInputFieldData.newPassword}
+            onChange={getInputFieldData}
+          />
           <form>
-            <input type="password" placeholder="Conform password" />
-            <button>Change</button>
+            <input
+              type="password"
+              placeholder="Conform password"
+              name="cNewPassword"
+              value={settingInputFieldData.cNewPassword}
+              onChange={getInputFieldData}
+            />
+            <button onClick={chnagePassword}>Change</button>
           </form>
           <p>Don't Forgot Your Password</p>
         </div>
@@ -40,7 +89,7 @@ const SettingPage = () => {
           <p>
             NOTE : You can be able to recover after you delete your user profile
           </p>
-          <button>Delete Profile</button>
+          <button onClick={deleteUser}>Delete Profile</button>
         </div>
       </div>
     </>
