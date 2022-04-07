03d4970681e6541f9caa4deafca9501339fb9ddf
Roman Ojha <razzroman98@gmail.com>
2022-04-07 19:56:17 +0545

    completed design of 404 page

--- client/src/pages/Page404.js
+++ client/src/pages/Page404.js
@@ -1,9 +1,19 @@
 import React from "react";
+import "../styles/pages/page404.css";
+import Img404 from "../assets/svg/404_not_found.svg";
 
 const Page404 = () => {
   return (
     <>
-      <div></div>
+      <div className="page404">
+        <div className="page404_Title_Button_Container">
+          <div className="page404_Title">
+            <h1>404</h1>
+          </div>
+          <button type="submit">Home</button>
+        </div>
+        {/* <img src={Img404} alt="404" /> */}
+      </div>
     </>
   );
 };
