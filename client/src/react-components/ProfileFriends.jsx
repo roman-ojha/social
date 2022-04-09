8693ca4b8a263f526118eac68a1457e6a2dfde02
Roman Ojha <razzroman98@gmail.com>
2022-04-09 18:20:36 +0545

    showing friends on profile page friends route

--- client/src/react-components/ProfileFriends.jsx
+++ client/src/react-components/ProfileFriends.jsx
@@ -1,10 +1,32 @@
 import React from "react";
-import { userProfileDetailAction } from "../redux-actions";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
+import User_Profile_Icon from "../Images/User_profile_Icon.svg";
+import "../styles/react-components/profileFriends.css";
 const ProfileFriends = () => {
   const profilePageData = useSelector((state) => state.profilePageDataReducer);
-
-  return <div></div>;
+  return (
+    <div className="ProfilePage_Friends_Container">
+      {profilePageData.friends.map((friendDetail, index) => {
+        return (
+          <div className="ProfilePage_Friend_Outline" key={index}>
+            <img
+              src={
+                friendDetail.picture === undefined
+                  ? User_Profile_Icon
+                  : friendDetail.picture
+              }
+              alt=""
+              className="ProfilePage_Friend_Image"
+            />
+            <p className="ProfilePage_Friend_Name">{friendDetail.userID}</p>
+            <div className="ProfilePage_Friend_Active_Status">
+              <p>Active</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default ProfileFriends;
