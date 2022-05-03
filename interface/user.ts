cd0f69fbe8000ff705dc23ce473cbe8695e57b46
Roman Ojha <razzroman98@gmail.com>
2022-05-03 13:58:30 +0545

    added User type of interface for Database

---
+++ interface/user.ts
@@ -0,0 +1,99 @@
+export default interface User {
+  googleID: number;
+  id: string;
+  name: string;
+  email: string;
+  picture: string;
+  password: string;
+  cpassword: string;
+  userID: string;
+  birthday: {
+    year: string;
+    month: string;
+    day: string;
+  };
+  date: Date;
+  gender: string;
+  messages: [
+    {
+      messageTo: string;
+      roomID: string;
+      receiverPicture: string;
+      message: [sender: string, content: string, date: Date];
+    }
+  ];
+  followersNo: number;
+  followers: [
+    {
+      email: string;
+      name: string;
+      userID: string;
+      picture: string;
+    }
+  ];
+  followingNo: number;
+  following: [
+    {
+      email: string;
+      name: string;
+      userID: string;
+      picture: string;
+    }
+  ];
+  friendsNo: {
+    type: number;
+  };
+  friends: [
+    {
+      email: string;
+      name: string;
+      userID: string;
+      picture: string;
+    }
+  ];
+  postNo: {
+    type: number;
+  };
+  posts: [
+    {
+      id: string;
+      caption: string;
+      picture: {
+        name: string;
+        path: string;
+        url: string;
+        firebaseStorageDownloadToken: string;
+        bucket: string;
+      };
+      likes: {
+        No: number;
+        by: [
+          {
+            userID: string;
+          }
+        ];
+      };
+      comments: {
+        No: number;
+        by: [
+          {
+            userID: string;
+            comment: string;
+            picture: string;
+          }
+        ];
+      };
+      date: Date;
+    }
+  ];
+  stories: {
+    caption: string;
+    picture: string;
+    date: string;
+  };
+  tokens: [
+    {
+      token: string;
+    }
+  ];
+}
--- models/userDetail_model.ts
+++ models/userDetail_model.ts
@@ -1,4 +1,9 @@
-export default {
+import { Schema, model } from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "../interface/user";
+
+const userDetailSchema = new Schema<User>({
   googleID: {
     type: Number,
   },
@@ -220,4 +225,85 @@ export default {
       },
     },
   ],
+});
+
+userDetailSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 12);
+    this.cpassword = await bcrypt.hash(this.cpassword, 12);
+  }
+  next();
+});
+
+userDetailSchema.methods.generateAuthToken = async function () {
+  try {
+    let token = jwt.sign({ _id: this._id }, process.env.SECRET_KEY);
+    this.tokens = this.tokens.concat({ token: token });
+    await this.save();
+    return token;
+  } catch (err) {
+    console.log(err);
+  }
 };
+
+userDetailSchema.methods.uploadPost = async function (
+  postData,
+  userStoryDetail
+) {
+  try {
+    // console.log(postData);
+    this.posts.unshift(postData);
+    if (userStoryDetail != undefined) {
+      this.stories = userStoryDetail;
+    }
+    this.postNo++;
+    await this.save();
+    return this.posts;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+userDetailSchema.methods.followUser = async function (followedToUser) {
+  try {
+    // saving following user detail into current user database
+    this.following.unshift(followedToUser);
+    this.followingNo++;
+    // saving following user detail into followed to user database
+    const res = await UserDetail.updateOne(
+      {
+        userID: followedToUser.userID,
+      },
+      {
+        // pushing the new followers into followed to user database
+        $push: {
+          followers: {
+            name: this.name,
+            email: this.email,
+            userID: this.userID,
+            picture: this.picture,
+          },
+        },
+        $inc: {
+          followersNo: 1,
+        },
+      }
+    );
+    if (res) {
+      await this.save();
+      return true;
+    } else {
+      return false;
+    }
+  } catch (err) {}
+};
+
+userDetailSchema.methods.saveMessage = async function (message) {
+  try {
+    console.log(message);
+    // searching for a use which is getting the message
+  } catch (err) {}
+};
+
+const UserDetail = model<User>("USERDETAIL", userDetailSchema);
+export default UserDetail;
