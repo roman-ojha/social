27550894b7f5f3663926bc8d02275f89aa3a5992
Roman Ojha <razzroman98@gmail.com>
2022-05-05 23:08:36 +0545

    added logic to fetch different type of youtube search result

--- services/api/youtubeAPI.ts
+++ services/api/youtubeAPI.ts
@@ -1,12 +1,15 @@
 import { instance as axios } from "../axios.js";
+import { google } from "googleapis";
 const API_KEY: string | undefined = process.env.YOUTUBE_BASE_API_KEY;
-const API_BASE_URL: string | undefined = process.env.YOUTUBE_API_URL;
+const youtube = google.youtube({
+  version: "v3",
+  auth: API_KEY,
+});
 
+const API_BASE_URL: string | undefined = process.env.YOUTUBE_API_URL;
 export default {
-  search: async (searchQuery: string) => {
-    console.log(searchQuery);
-    const url = `${API_BASE_URL}/search?key=${API_KEY}&type=video&part=snippet&q=${searchQuery}`;
-    console.log(url);
+  search: async (searchQuery: string, maxResults: Number) => {
+    const url = `${API_BASE_URL}/search?key=${API_KEY}&type=video&part=snippet&q=${searchQuery}&maxResults=${maxResults}`;
     return await axios({
       method: "GET",
       url: url,
