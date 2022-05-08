9cd1ecae136114475fad06f2e047fa4c15b47659
Roman Ojha <razzroman98@gmail.com>
2022-05-08 12:14:16 +0545

    panding the logic for youtube video serach

--- controllers/scrap.controller.ts
+++ controllers/scrap.controller.ts
@@ -75,63 +75,63 @@ export default {
     }
   },
 
-  youtubeSearch: async (req: Request, res: Response) => {
-    try {
-      const { q } = req.query;
-      if (!q) {
-        return res.status(400).json(<ResponseObject>{
-          success: false,
-          msg: "Please fill the search field",
-        });
-      }
-      const [searchPage, closeSearchPage] = await newPage();
-      const url: string = `https://www.youtube.com/results?search_query=${q}`;
-      if (searchPage) {
-        await searchPage.goto(url, {
-          waitUntil: "networkidle0",
-        });
-      }
-      if (!searchPage || !closeSearchPage) {
-        return res.status(500).json(<ResponseObject>{
-          success: false,
-          msg: "Server Error, Please try again later",
-        });
-      }
-      //   waiting for document to get load
-      await searchPage.waitForSelector("ytd-rich-grid-media");
-      const videos = await searchPage.evaluate(() => {
-        return Array.from(
-          document.querySelectorAll("ytd-video-renderer a#video-title")
-        ).map((el) => {
-          const href = el.getAttribute("href");
-          const title = el.getAttribute("title");
-          if (href && title) {
-            return {
-              videoId: href.slice(9, href.length),
-              title: el.getAttribute("title"),
-            };
-          }
-        });
-      });
-      if (videos.length > 0) {
-        await closeSearchPage();
-        return res.status(200).json(<ResponseObject>{
-          success: true,
-          msg: "Successful",
-          videos,
-        });
-      } else {
-        await closeSearchPage();
-        return res.status(404).json(<ResponseObject>{
-          success: false,
-          msg: "Can't be able to get Youtube data, Please try again later",
-        });
-      }
-    } catch (err) {
-      return res.status(500).json(<ResponseObject>{
-        success: false,
-        msg: "Server Error, Please try again later",
-      });
-    }
-  },
+  // youtubeSearch: async (req: Request, res: Response) => {
+  //   try {
+  //     const { q } = req.query;
+  //     if (!q) {
+  //       return res.status(400).json(<ResponseObject>{
+  //         success: false,
+  //         msg: "Please fill the search field",
+  //       });
+  //     }
+  //     const [searchPage, closeSearchPage] = await newPage();
+  //     const url: string = `https://www.youtube.com/results?search_query=${q}`;
+  //     if (searchPage) {
+  //       await searchPage.goto(url, {
+  //         waitUntil: "networkidle0",
+  //       });
+  //     }
+  //     if (!searchPage || !closeSearchPage) {
+  //       return res.status(500).json(<ResponseObject>{
+  //         success: false,
+  //         msg: "Server Error, Please try again later",
+  //       });
+  //     }
+  //     //   waiting for document to get load
+  //     await searchPage.waitForSelector("ytd-rich-grid-media");
+  //     const videos = await searchPage.evaluate(() => {
+  //       return Array.from(
+  //         document.querySelectorAll("ytd-video-renderer a#video-title")
+  //       ).map((el) => {
+  //         const href = el.getAttribute("href");
+  //         const title = el.getAttribute("title");
+  //         if (href && title) {
+  //           return {
+  //             videoId: href.slice(9, href.length),
+  //             title: el.getAttribute("title"),
+  //           };
+  //         }
+  //       });
+  //     });
+  //     if (videos.length > 0) {
+  //       await closeSearchPage();
+  //       return res.status(200).json(<ResponseObject>{
+  //         success: true,
+  //         msg: "Successful",
+  //         videos,
+  //       });
+  //     } else {
+  //       await closeSearchPage();
+  //       return res.status(404).json(<ResponseObject>{
+  //         success: false,
+  //         msg: "Can't be able to get Youtube data, Please try again later",
+  //       });
+  //     }
+  //   } catch (err) {
+  //     return res.status(500).json(<ResponseObject>{
+  //       success: false,
+  //       msg: "Server Error, Please try again later",
+  //     });
+  //   }
+  // },
 };
