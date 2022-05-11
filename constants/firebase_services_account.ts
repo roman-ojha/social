18232ffd40fcca98442b76df17887c3e7b935d8d
Roman Ojha <razzroman98@gmail.com>
2022-05-11 23:45:51 +0545

    updating firebase_services_account.ts cause heroku got error

--- constants/firebase_services_account.ts
+++ constants/firebase_services_account.ts
@@ -2,7 +2,7 @@ export default {
   type: "service_account",
   project_id: process.env.FIREBASE_PROJECT_ID,
   private_key_id: process.env.FIREBASE_PROJECT_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_CLIENT_ID,
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
