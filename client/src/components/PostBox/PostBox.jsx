effbfb714e4565df50a4e3c6a71f1dc08de0a7dd
Roman Ojha <razzroman98@gmail.com>
2022-05-24 17:57:29 +0545

    separated LikeCommentShare comp from PostBox (#83)

--- client/src/components/PostBox/PostBox.jsx
+++ client/src/components/PostBox/PostBox.jsx
@@ -4,7 +4,6 @@ import { useDispatch, useSelector } from "react-redux";
 import { Icon } from "@iconify/react";
 import "../../styles/components/postBox.css";
 import {
-  commentBoxAction,
   stopProgressBar,
   startProgressBar,
 } from "../../services/redux-actions";
@@ -14,6 +13,7 @@ import Api from "../../services/api/components/postBox";
 import PostImage from "./PostImage";
 import PostCaption from "./PostCaption";
 import PostInfo from "./PostInfo";
+import LikeCommentShare from "./LikeCommentShare";
 
 const PostBox = (props) => {
   const dispatch = useDispatch();
@@ -32,46 +32,12 @@ const PostBox = (props) => {
     isLikedPost: props.userFeedData.likes.by.some(
       (el) => el.userID === userProfileDetailStore.userID
     ),
-    likeNo: props.userFeedData.likes.No,
-    commentNo: props.userFeedData.comments.No,
     postCommentInfo:
       props.userFeedData.comments.by[
         Math.floor(Math.random() * props.userFeedData.comments.by.length)
       ],
   });
 
-  const like = async () => {
-    try {
-      const res = await Api.like({
-        postID: props.userFeedData.id,
-        toUserId: props.userMainInformation.userID,
-        toId: props.userMainInformation.id,
-        likeNo: postInformation.likeNo,
-      });
-      const data = await res.data;
-      if (data.success && res.status === 200 && !data.removed) {
-        // Liked the post
-        setPostInformation({
-          ...postInformation,
-          likeNo: data.likeNo,
-          isLikedPost: true,
-        });
-      } else if (data.success && res.status === 200 && data.removed) {
-        // Removed Like from the post Post
-        setPostInformation({
-          ...postInformation,
-          likeNo: data.likeNo,
-          isLikedPost: false,
-        });
-      }
-    } catch (err) {
-      if (err.response.data.success === false) {
-        toastError(err.response.data.msg);
-      } else {
-        toastError("Some Problem Occur, Please Try again later!!!");
-      }
-    }
-  };
   const comment = async () => {
     try {
       dispatch(startProgressBar());
@@ -110,36 +76,6 @@ const PostBox = (props) => {
       dispatch(stopProgressBar());
     }
   };
-  const getComment = async () => {
-    try {
-      dispatch(startProgressBar());
-      const res = await Api.getComment({
-        postID: props.userFeedData.id,
-        userID: props.userMainInformation.userID,
-      });
-      const data = await res.data;
-      if (res.status === 200 && data.success) {
-        dispatch(
-          commentBoxAction({
-            openCommentBox: true,
-            postID: props.userFeedData.id,
-            to: props.userMainInformation.userID,
-            comments: data.comment.by,
-          })
-        );
-      } else {
-        toastError(data.msg);
-      }
-      dispatch(stopProgressBar());
-    } catch (err) {
-      if (err.response.data.success === false) {
-        toastError(err.response.data.msg);
-      } else {
-        toastError("Some Problem Occur, Please Try again later!!!");
-      }
-      dispatch(stopProgressBar());
-    }
-  };
 
   return (
     <>
@@ -154,40 +90,10 @@ const PostBox = (props) => {
             postDate={props.userFeedData.date}
           />
           <div className="HomePage_Feed_Love_Comment_Share_Info_Container">
-            <div className="HomePage_Feed_Icon_Container" onClick={like}>
-              {postInformation.isLikedPost ? (
-                <Icon
-                  className="HomePage_Feed_Liked_Love_Icon"
-                  icon="fluent:thumb-like-20-filled"
-                />
-              ) : (
-                <Icon
-                  className="HomePage_Feed_UnLiked_Love_Icon"
-                  icon="fluent:thumb-like-20-regular"
-                />
-              )}
-              <p>{postInformation.likeNo}</p>
-            </div>
-            <div className="HomePage_Feed_Icon_Container">
-              <Icon
-                className="HomePage_Feed__Comment_Icon"
-                icon="akar-icons:comment"
-                onClick={() => {
-                  // dispatch(
-                  //   commentBoxAction({
-                  //     openCommentBox: true,
-                  //     postID: props.userFeedData.id,
-                  //     to: props.userMainInformation.userID,
-                  //     comments: props.userFeedData.comments.by,
-                  //   })
-                  // );
-                  getComment();
-                }}
-              />
-              <p>{postInformation.commentNo}</p>
-            </div>
-            <Icon className="HomePage_Feed_Share_Icon" icon="bx:share" />
-            <Icon className="HomePage_Feed_More_Info_Icon" icon="ep:more" />
+            <LikeCommentShare
+              userFeedData={props.userFeedData}
+              userMainInformation={props.userMainInformation}
+            />
           </div>
         </div>
         <div className="UserPostFeed_Comment_Box">
