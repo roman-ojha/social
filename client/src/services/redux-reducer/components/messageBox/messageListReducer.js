bfaae59ae5ed7976a99a2fc9c7ec6bef904409aa
Roman Ojha <razzroman98@gmail.com>
2022-05-27 19:21:52 +0545

    fix(sort_message): completed sorting message list
    fixes #94

--- client/src/services/redux-reducer/components/messageBox/messageListReducer.js
+++ client/src/services/redux-reducer/components/messageBox/messageListReducer.js
@@ -2,8 +2,14 @@ const initialState = [];
 
 const messageListReducer = (state = initialState, action) => {
   if (action.type === "messageList") {
-    console.log(action.payload);
-    return action.payload;
+    return action.payload.sort((a, b) => {
+      if (new Date(a.lastMessageOn) < new Date(b.lastMessageOn)) {
+        console.log("yes");
+        return 1;
+      } else {
+        return -1;
+      }
+    });
   } else if (action.type === "appendMessageOnMessageList") {
     return state.map((element) =>
       element.messageToId === action.payload.receiverId
