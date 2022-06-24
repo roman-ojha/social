92610741cc78c5d5ec27e4f57db87b2b090a4eae
Roman-Ojha <razzroman98@gmail.com>
2022-06-24 13:42:06 +0545

    fix: configure javascript to typescript
    fixing #109

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App.js";
+import App from "./App";
 import "./App.css";
+import "./styles/variables.css";
+import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./services/redux-store/store";
-import "./styles/variables.css";
-import "./index.css";
+
 ReactDOM.render(
   <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
   </BrowserRouter>,
+
   document.getElementById("root")
 );
