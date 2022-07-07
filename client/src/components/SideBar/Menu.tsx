a2ced02b1d736eeac39f42a0560c091b80fb3ad8
Roman-Ojha <razzroman98@gmail.com>
2022-07-07 09:53:01 +0545

    style: added Button on all SideBar Menu Item
    fixing #117

--- client/src/components/SideBar/Menu.tsx
+++ client/src/components/SideBar/Menu.tsx
@@ -71,63 +71,91 @@ const Menu = ({ classes, ...other }): JSX.Element => {
               <h3 className="MainPage_SideBar_Menu_Home_Title">Home</h3>
             </NavLink>
           </Button>
-          <NavLink
-            to="/u/video"
-            className="MainPage_SideBar_Menu_Video_Container MainPage_SideBar_Link"
+          <Button
+            // TouchRippleProps={{ classes: rippleClasses }}
+            // className={buttonClass}
+            // {...other}
+            id="MainPage_SideBar_Menu_MUI_Button"
           >
-            <div className="MainPage_SideBar_Menu_SelectBar_Colored"></div>
-            <Icon
-              className="MainPage_SideBar_Menu_Home_Logo MainPage_SideBar_Menu_Logo"
-              icon="clarity:video-gallery-solid"
-            />
-            <h3 className="MainPage_SideBar_Menu_Video_Title">Video</h3>
-          </NavLink>
-          <NavLink
-            to="/u/message"
-            className="MainPage_SideBar_Menu_Message_Container MainPage_SideBar_Link"
+            <NavLink
+              to="/u/video"
+              className="MainPage_SideBar_Menu_Video_Container MainPage_SideBar_Link"
+            >
+              <div className="MainPage_SideBar_Menu_SelectBar_Colored"></div>
+              <Icon
+                className="MainPage_SideBar_Menu_Home_Logo MainPage_SideBar_Menu_Logo"
+                icon="clarity:video-gallery-solid"
+              />
+              <h3 className="MainPage_SideBar_Menu_Video_Title">Video</h3>
+            </NavLink>
+          </Button>
+          <Button
+            // TouchRippleProps={{ classes: rippleClasses }}
+            // className={buttonClass}
+            // {...other}
+            id="MainPage_SideBar_Menu_MUI_Button"
           >
-            <div className="MainPage_SideBar_Menu_SelectBar_Colored"></div>
-            <Icon
-              className="MainPage_SideBar_Menu_Home_Logo MainPage_SideBar_Menu_Logo"
-              icon="ant-design:message-filled"
-            />
-            <h3 className="MainPage_SideBar_Menu_Message_Title">Message</h3>
-          </NavLink>
-          <NavLink
-            to="/u/setting"
-            className="MainPage_SideBar_Menu_Setting_Container MainPage_SideBar_Link"
+            <NavLink
+              to="/u/message"
+              className="MainPage_SideBar_Menu_Message_Container MainPage_SideBar_Link"
+            >
+              <div className="MainPage_SideBar_Menu_SelectBar_Colored"></div>
+              <Icon
+                className="MainPage_SideBar_Menu_Home_Logo MainPage_SideBar_Menu_Logo"
+                icon="ant-design:message-filled"
+              />
+              <h3 className="MainPage_SideBar_Menu_Message_Title">Message</h3>
+            </NavLink>
+          </Button>
+          <Button
+            // TouchRippleProps={{ classes: rippleClasses }}
+            // className={buttonClass}
+            // {...other}
+            id="MainPage_SideBar_Menu_MUI_Button"
           >
-            <div className="MainPage_SideBar_Menu_SelectBar_Colored"></div>
-            <Icon
-              className="MainPage_SideBar_Menu_Home_Logo MainPage_SideBar_Menu_Logo"
-              icon="ant-design:setting-filled"
-            />
-            <h3 className="MainPage_SideBar_Menu_Setting_Title">Setting</h3>
-          </NavLink>
-          <NavLink
-            to={`/u/profile/${userProfileDetailStore.userID}/posts`}
-            className="MainPage_SideBar_Menu_Profile_Container MainPage_SideBar_Link"
-            onClick={() => {
-              const userObj = {
-                ...userProfileDetailStore,
-                isRootUserFollowed: false,
-              };
-              profilePageDataAction(userObj);
-              if (!rootUserProfileDataState.fetchedRootUserProfileData) {
-                setRootUserProfileDataState({
-                  fetchedRootUserProfileData: false,
-                  getRootUserProfileData: true,
-                });
-              }
-            }}
+            <NavLink
+              to="/u/setting"
+              className="MainPage_SideBar_Menu_Setting_Container MainPage_SideBar_Link"
+            >
+              <div className="MainPage_SideBar_Menu_SelectBar_Colored"></div>
+              <Icon
+                className="MainPage_SideBar_Menu_Home_Logo MainPage_SideBar_Menu_Logo"
+                icon="ant-design:setting-filled"
+              />
+              <h3 className="MainPage_SideBar_Menu_Setting_Title">Setting</h3>
+            </NavLink>
+          </Button>
+          <Button
+            // TouchRippleProps={{ classes: rippleClasses }}
+            // className={buttonClass}
+            // {...other}
+            id="MainPage_SideBar_Menu_MUI_Button"
           >
-            <div className="MainPage_SideBar_Menu_SelectBar_Colored"></div>
-            <Icon
-              className="MainPage_SideBar_Menu_Home_Logo MainPage_SideBar_Menu_Logo"
-              icon="gg:profile"
-            />
-            <h3 className="MainPage_SideBar_Menu_Profile_Title">Profile</h3>
-          </NavLink>
+            <NavLink
+              to={`/u/profile/${userProfileDetailStore.userID}/posts`}
+              className="MainPage_SideBar_Menu_Profile_Container MainPage_SideBar_Link"
+              onClick={() => {
+                const userObj = {
+                  ...userProfileDetailStore,
+                  isRootUserFollowed: false,
+                };
+                profilePageDataAction(userObj);
+                if (!rootUserProfileDataState.fetchedRootUserProfileData) {
+                  setRootUserProfileDataState({
+                    fetchedRootUserProfileData: false,
+                    getRootUserProfileData: true,
+                  });
+                }
+              }}
+            >
+              <div className="MainPage_SideBar_Menu_SelectBar_Colored"></div>
+              <Icon
+                className="MainPage_SideBar_Menu_Home_Logo MainPage_SideBar_Menu_Logo"
+                icon="gg:profile"
+              />
+              <h3 className="MainPage_SideBar_Menu_Profile_Title">Profile</h3>
+            </NavLink>
+          </Button>
         </div>
         <hr className="MainPage_SideBar_Horizontal_Line" />
       </nav>
